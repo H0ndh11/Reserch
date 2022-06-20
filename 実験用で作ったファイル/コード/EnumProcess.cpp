c6e80b60c1eb891bd2d0dae527f3f44a04a90d93
Honda <keisuke.honda.8f@stu.hosei.ac.jp>
2022-06-20 12:01:55 +0900

    プロセス表示まで実装

--- 実験用で作ったファイル/コード/EnumProcess.cpp
+++ 実験用で作ったファイル/コード/EnumProcess.cpp
@@ -2,10 +2,33 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <psapi.h>
+#include <string>
+
+using namespace std;
 
 //DWORDはunsigned longと同じ
-void PrintProcessNameAndID(DWORD processID) {
-	//プロセス名とプロセス識別子の表示
+void FindSandboxFromProcessName(DWORD processID) {
+	//virtualboxを検知する．ここを変えれば他のプロセス名を検知することが可能
+	string blacklist[3] = { "vb","vbox", "virtualbox"};
+
+	//TCHARはコンパイルオプションで通常とユニコードを切り替えできるchar型
+	//szはunknownで初期化し，最終的にプロセス名が入る
+	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+
+	//プロセス名を取得
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+	if (NULL != hProcess) {
+		HMODULE hMod;
+		DWORD cbNeeded;
+		if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded)) {
+			GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
+		}
+	}
+
+	//全てのプロセス名とプロセスIDをターミナル上に表示
+	_tprintf(TEXT("%s (PID: %u)\n"), szProcessName, processID);
+
+	CloseHandle(hProcess);
 }
 
 //メイン
@@ -15,21 +38,21 @@ int main() {
 
 	/*
 	32ビット限定 EnumProcesses
-	aProcesses配列にプロセス名が入る
+	aProcesses配列にプロセスIDが入る
 	cbNeededにバイト数が入る
 	*/
 	if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
 		return 1;
 	}
 
-	//プロセス識別子がいくつ返されたか計算
+	//プロセスIDがいくつ返されたか計算
 	//返されたバイト数/DWORDサイズ(4)
 	cProcesses = cbNeeded / sizeof(DWORD);
 
-	//プロセス名とプロセス識別子の表示
+	//1つずつプロセスIDをPrintProcessNameAndIDに渡す
 	for (i = 0; i < cProcesses; i++) {
 		if (aProcesses[i] != 0) {
-			PrintProcessNameAndID(aProcesses[i]);
+			FindSandboxFromProcessName(aProcesses[i]);
 		}
 	}
 
