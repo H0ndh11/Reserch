2e9792599c369bb5420fedb043b764ce5f22c660
H0ndh11 <keisuke.honda.8f@stu.hosei.ac.jp>
2023-11-25 20:48:08 +0900

    add new file

---
+++ 実験用で作ったファイル/コード/MEMsize.cpp
@@ -0,0 +1,22 @@
+﻿#include "framework.h"
+#include "MEMsize.h"
+#include <windows.h>
+#include <iostream>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    //ハードディスクサイズが100GBより大きかった場合に活性化
+    MEMORYSTATUSEX memInfo;
+    memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+    GlobalMemoryStatusEx(&memInfo);
+    if (memInfo.ullTotalPhys > 4000000000) {
+            MessageBox(NULL, TEXT("マルウェアが活性化しました"), TEXT("Time of Attack!"), MB_OK | MB_ICONERROR);
+    }
+    return 0;
+}
