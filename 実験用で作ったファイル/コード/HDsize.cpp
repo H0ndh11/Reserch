8502eee3746840f860907ecce35758994bb56d52
H0ndh11 <keisuke.honda.8f@stu.hosei.ac.jp>
2023-11-25 15:39:50 +0900

    add new file

---
+++ 実験用で作ったファイル/コード/HDsize.cpp
@@ -0,0 +1,22 @@
+﻿#include "framework.h"
+#include "HDsize.h"
+#include <windows.h>
+#include <iostream>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    //ハードディスクサイズが100GBより大きかった場合に活性化
+    ULARGE_INTEGER freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes;
+    if (GetDiskFreeSpaceEx(NULL, &freeBytesAvailable, &totalNumberOfBytes, &totalNumberOfFreeBytes)) {
+        if (totalNumberOfBytes.QuadPart > 100000000000) {
+            MessageBox(NULL, TEXT("マルウェアが活性化しました"), TEXT("Time of Attack!"), MB_OK | MB_ICONERROR);
+        }
+    }
+    return 0;
+}
